fae0e4ebbb1c9f9eabcc559439cb2c0053a6f4df
John Blackbourn <johnbillion@gmail.com>
2015-12-13 02:18:23 +0000

    Mock a stub request so we can test the request properties.

--- test/tests/RequestTest.php
+++ test/tests/RequestTest.php
@@ -12,15 +12,42 @@ class RequestTest extends PHPUnit_Framework_TestCase {
 	public function test_request_signing() {
 		$request = new StubRequest( 'https://acme.example.org' );
 
-		$request->set_request_body( [
+		$body = [
 			'hello' => 'world',
-		] );
+		];
+		$request->set_request_body( $body );
 		$request->set_request_nonce( 'nonce' );
 
 		$keypair = new \LEWP\Keys\KeyPair( 'test' );
 		$keypair->generate( 'foo' );
 
 		$request->sign( $keypair, 'foo' );
+
+		$args = [
+			'body'    => $request->get_signature(),
+			'headers' => $request->get_request_headers(),
+		];
+
+		// Mock the remote request
+		\WP_Mock::wpFunction( 'wp_remote_request', array(
+			'args'   => [
+				'https://acme.example.org/stub',
+				$args,
+			],
+			'times'  => 1,
+			'return' => [], // of no concern
+		) );
+
+		$request->send();
+
+		$request_args = $request->get_request_args();
+
+		$this->assertNotEquals( $body, $request_args['body'] );
+		$this->assertArrayHasKey( 'header',    $request_args['body'] );
+		$this->assertArrayHasKey( 'protected', $request_args['body'] );
+		$this->assertArrayHasKey( 'payload',   $request_args['body'] );
+		$this->assertArrayHasKey( 'signature', $request_args['body'] );
+
 	}
 
 }
